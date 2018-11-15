36e03cec33cbd9740a1b90bbd3e04d74dda9ab49
malithmcr <malith.priyashan.dev@gmail.com>
2018-11-15 08:40:42 +0100

    added some styling

--- frontend/src/app/questions/questions.component.ts
+++ frontend/src/app/questions/questions.component.ts
@@ -9,9 +9,33 @@ import { QuestionsService } from '../questions.service';
 export class QuestionsComponent implements OnInit {
 
   public questions = [];
+  score = 0;
+  answerSelected : number;
+  isCorrectAnswer: number;
+  activeQuestion = 0;
+  answerdQuestions = 0;
+  Answers = null;
 
   constructor(private api: QuestionsService) { }
-  
+  nextQuestion() {
+    this.answerSelected = null;
+    if(this.activeQuestion !== this.questions.length) {
+      this.activeQuestion++
+    }
+  }
+
+  selectedQuestion(selected) {
+    let userAnswer = selected;
+    
+    if(this.questions[this.activeQuestion].Correct === userAnswer) {
+      this.score = this.score + 10;
+      this.isCorrectAnswer = selected;
+    
+    } else {
+      this.answerSelected = selected;
+    }
+    
+  }
   ngOnInit() {
     this.api.getQuestions()
       .subscribe( data => this.questions = data );
